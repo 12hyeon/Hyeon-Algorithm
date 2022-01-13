bc9bbc84784db990987700c813f728c6b8f17db0
HyeonJeong <67951802+12eon@users.noreply.github.com>
2022-01-13 16:28:46 +0900

    successful algorithm problem
    Most of the problems were solved using java.

---
+++ BOJ/10869/arithmetic_operation.java
@@ -0,0 +1,17 @@
+package BOJ;
+import java.util.Scanner;
+
+public class arithmetic_operation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x, y;
+        x = sc.nextInt();
+        y = sc.nextInt();
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+        System.out.println(x%y);
+        sc.close();
+    }
+}
